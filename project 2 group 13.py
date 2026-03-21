4b2cd971cf98e2389362fa7e0807c0504bb8d47e
KarlProgs <karl.alvarez2t1@gmail.com>
2026-03-21 19:29:57 -0400

    add part 2

--- project 2 group 13.py
+++ project 2 group 13.py
@@ -1,5 +1,54 @@
+import random
 import socket
 import sys
+
+
+def build_dns_query(domain_name):
+    query = bytearray(b"")
+
+    ##########
+    # HEADER #
+    ##########
+    transaction_id = random.randint(0, 0xFFFF)
+    query += transaction_id.to_bytes(2, 'big')
+
+    # A query packet without any special options will have its flags field simply be set to 0
+    flags = (0).to_bytes(2, 'big')
+    query += flags
+
+    num_questions = (1).to_bytes(2, 'big')
+    query += num_questions
+
+    num_answers = (0).to_bytes(2, 'big')
+    query += num_answers
+
+    num_authority_rrs = (0).to_bytes(2, 'big')
+    query += num_authority_rrs
+
+    num_additional_rrs = (0).to_bytes(2, 'big')
+    query += num_additional_rrs
+
+    ############
+    # QUESTION #
+    ############
+    # Encode the domain_name into bytes
+    encoded_domain_name = b""
+    for part in domain_name.encode("ascii").split(b"."):
+        encoded_domain_name += bytes([len(part)]) + part
+    encoded_domain_name += b"\x00"
+
+    qname = encoded_domain_name
+    query.extend(qname)
+
+    # QTYPE (1 = A record)
+    qtype = (1).to_bytes(2, 'big')
+    query += qtype
+
+    # QCLASS (1 = IN [the internet])
+    qclass = (1).to_bytes(2, 'big')
+    query += qclass
+
+    return query, transaction_id
 
 
 def send_dns_query(domain_name, root_server_ip):
