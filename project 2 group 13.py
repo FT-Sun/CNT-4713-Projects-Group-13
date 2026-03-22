685d1e80d4ea1a3a141af426de4660a885521e7d
Sun <150195220+LunarSun2114@users.noreply.github.com>
2026-03-21 20:30:42 -0400

    Update project 2 group 13.py

--- project 2 group 13.py
+++ project 2 group 13.py
@@ -55,6 +55,9 @@ def send_dns_query(domain_name, root_server_ip):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.settimeout(5)
 
+       # Use Part 2 (Builder)
+    query, tx_id = build_dns_query(domain_name)
+
     # Below this is where the Query builder code should be (make sure to delete this once implemented)
     # Example:
     # dns_query = build_dns_query(domain_name)
@@ -66,11 +69,18 @@ def send_dns_query(domain_name, root_server_ip):
         print(f"Sending DNS query to {root_server_ip} ...")
         sock.sendto(dns_query, server_address)
 
+        sock.sendto(query, (server_ip, 53))
+        data, _ = sock.recvfrom(4096)
+
         # This is where the reply should be read from the Response Parser, delete once implemented
+
+    # Use Part 3 (Parser)
+        ans, auth, add = parse_response(data)
+        return ans, auth, add
 
     except socket.timeout:
         print("DNS server did not respond (timeout).")
-        return None
+        return None, None, None
 
     finally:
         sock.close()
